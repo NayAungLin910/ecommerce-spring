696cb041160e1d88e98c0df1219a6345ba865272
nayaunglin910@gmail.com <nayaunglin910@gmail.com>
2024-03-30 10:15:59 +0630

    Add JwtUtil Class

---
+++ src/main/java/com/ecommerce/javaspring/ecom/utils/JwtUtili.java
@@ -0,0 +1,52 @@
+package com.ecommerce.javaspring.ecom.utils;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtili {
+
+	public transient static final String SECRET = "692C00B4E4B2FEFF30C2C8D14F821488F66D14D50CB8736E80AF9CC2F53DAF3A";
+
+	public String generateToken(String userName) {
+		Map<String, Object> claims = new HashMap<>();
+		return createToken(claims, userName);
+	}
+
+	private String createToken(Map<String, Object> claims, String userName) {
+		return Jwts.builder().setClaims(claims).setSubject(userName).setIssuedAt(new Date(System.currentTimeMillis()))
+				.setExpiration(new Date(System.currentTimeMillis() + 10000 * 60 * 30))
+				.signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
+	}
+
+	private SecretKey getSignKey() {
+		byte[] keybytes = Decoders.BASE64.decode(SECRET);
+		return Keys.hmacShaKeyFor(keybytes);
+	}
+
+	public String extractUsername(String token) {
+		return extractClaim(token, Claims::getSubject);
+	}
+
+	public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+		final Claims claims = extractAllClaims(token);
+		return claimsResolver.apply(claims);
+	}
+
+	private Claims extractAllClaims(String token) {
+		return Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token).getBody();
+	}
+}
